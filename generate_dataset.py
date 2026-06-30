b93505f8f3f3295e195848aab975a8f75be3acfc
akngua2206-dot <akngua2206@gmail.com>
2026-07-01 01:36:15 +0700

    Update generate_dataset.py

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,1 +1,39 @@
+# ==========================================================
+# SMART POS AI DATASET GENERATOR
+# Version : 2.0
+# Author  : SmartPOS AI
+# ==========================================================
 
+import os
+import random
+import zipfile
+from datetime import datetime, timedelta
+
+import numpy as np
+import pandas as pd
+
+random.seed(42)
+np.random.seed(42)
+
+# ==========================================================
+# MEMBUAT FOLDER DATASET
+# ==========================================================
+
+OUTPUT_FOLDER = "dataset"
+
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+
+print("Folder dataset siap.")
+
+# ==========================================================
+# KONFIGURASI
+# ==========================================================
+
+JUMLAH_HARI = 30
+
+TRANSAKSI_MIN = 30
+
+TRANSAKSI_MAX = 45
+
+print("Konfigurasi selesai.")
